bfe9e6429566d2054d99bbd2fe437ccf6fc9e46d
AO554 <24486206+AO554@users.noreply.github.com>
2022-07-28 03:29:00 +0100

    Core Stuff [1.0.0]

---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MelonLoader;
+using MelonLoader.ICSharpCode.SharpZipLib.Zip;
+
+namespace CVRConsoleChange
+{
+    public class Main : MelonMod
+    {
+        internal static readonly MelonLogger.Instance log = new MelonLogger.Instance("CVRConsoleChange", ConsoleColor.Magenta);
+        public static MelonPreferences_Category CVRConsoleChange;
+        public static MelonPreferences_Entry<bool> Enabled;
+        public static MelonPreferences_Entry<string> WindowTitleStr;
+
+        public override void OnApplicationStart()
+        {
+            log.Msg("Change Process Window in Progress...");
+            
+            // Generate MelonLoader Preferences for this Mod
+            CVRConsoleChange = MelonPreferences.CreateCategory("CVRConsoleChange", "CVRConsoleChange");
+            Enabled = CVRConsoleChange.CreateEntry("Enabled", true);
+            WindowTitleStr = CVRConsoleChange.CreateEntry("WindowTitle", "ChilloutVR Console");
+            
+            // Change Title to String...duh
+            if (Enabled.Value == true)
+            {
+                Console.Title = (WindowTitleStr.Value);
+                log.Msg("Title updated!");
+            }
+            else
+                log.Msg("Retaining Default Console Title.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+
+// Main Mod Information
+[assembly: AssemblyTitle("CVRConsoleChange")]
+[assembly: AssemblyDescription("Change the Title of the MelonLoader Window.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CVRConsoleChange")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//General Stuff
+[assembly: ComVisible(false)]
+[assembly: Guid("b5774712-43a1-44ee-843e-af11fec9ddbe")]
+
+// Version
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//Author Information & Game Information
+[assembly: MelonInfo(typeof(CVRConsoleChange.Main), "CVRConsoleChange", "1.0.0", "AO554")]
+[assembly: MelonGame(null, null)]
